395a657f41cbace83f3201c3ebbeb0ad8beaab50
linyuchao123 <476638303@qq.com>
2026-08-02 16:24:15 +0800

    pyecharts模块简介
    Signed-off-by: linyuchao123 <476638303@qq.com>

---
+++ 10_python案例/03-pyecharts模块简介.py
@@ -0,0 +1,11 @@
+# 1、可视化图表开发技术栈
+# 使用ECharts的Python适配库：PyEcharts
+
+# 2、PyEcharts安装命令
+# pip install pyecharts
+
+# 3、官方示例画廊地址
+# https://gallery.pyecharts.org/#/README
+
+# 导入pyecharts模块
+import pyecharts
